3dbaade7d148d8b9c4655c6546a328dbe6db4a9b
Heitor Ramos <heitorhfbr@gmail.com>
2020-04-14 10:25:39 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/heitor/course/config/TestConfig.java
+++ src/main/java/com/heitor/course/config/TestConfig.java
@@ -37,25 +37,42 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Heitor Ramos", "heitor@gmail.com", "99009909", "123123");
+		
+		useRepository.saveAll(Arrays.asList(u1, u2, u3));
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.WATTING_PAYMENTS);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WATTING_PAYMENTS);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.PAID);
+		
+		orderRepositoty.saveAll(Arrays.asList(o1, o2, o3));
 
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
-		useRepository.saveAll(Arrays.asList(u1, u2, u3));
-		orderRepositoty.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		productsRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat1);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productsRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
+		
+		
+		
 	}
 
 }
--- src/main/java/com/heitor/course/entity/Product.java
+++ src/main/java/com/heitor/course/entity/Product.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 
 
@@ -26,7 +28,9 @@ public class Product implements Serializable{
 	private String Description;
 	private Double price;
 	private String imgUrl;
-	@Transient
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
@@ -112,17 +116,8 @@ public class Product implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", Description=" + Description + ", price=" + price
-				+ ", imgUrl=" + imgUrl + ", categories=" + categories + "]";
+		return "Product [id=" + id + ", name=" + name + ", Description=" + Description + ", price=" + price + ", imgUrl=" + imgUrl + ", categories=" + categories + "]";
 	}
 
-
-	
-	
-	
-	
-	
-	
-	
 	
 }
